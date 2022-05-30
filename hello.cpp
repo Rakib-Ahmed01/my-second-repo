8628dd3523644c59036a161a20fa0ac2957217d0
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2022-05-30 23:12:16 +0600

    Added add & sub function

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int add(int n, int m, int o)
+int multiply(int n, int m)
 {
-    return n + m + o;
+    return n * m;
 }
-
 int sub(int n, int m, int o)
 {
     return n - (m - o);
