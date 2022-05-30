a8e8a3a1d749561610c31359712088166301bca9
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2022-05-30 23:07:33 +0600

    Added add & sub function

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int add(int n, int m)
+{
+    return n + m;
+}
+
+int sub(int n, int m)
+{
+    return n - m;
+}
+
 int main()
 {
     cout << "Assalamualaikum.Kmon Achen..?" << endl;
